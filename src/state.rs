8b1eef4b156d00c479893dbc94a1296f8e0ac669
Shark-empire <babor3331@icloud.com>
2026-08-21 20:35:36 +0000

    Add SeatState to MitosGuiState structure

--- src/state.rs
+++ src/state.rs
@@ -1,15 +1,17 @@
-//! Global state for the MITOS compositor.
-
-use smithay::wayland::{
-    compositor::CompositorState,
-    shm::ShmState,
-    shell::xdg::XdgShellState,
+use smithay::{
+    input::SeatState,
+    wayland::{
+        compositor::CompositorState,
+        shm::ShmState,
+        shell::xdg::XdgShellState,
+    },
 };
 
 pub struct MitosGuiState {
     pub compositor_state: CompositorState,
     pub xdg_shell_state: XdgShellState,
     pub shm_state: ShmState,
+    pub seat_state: SeatState<Self>,
 }
 
 impl MitosGuiState {
@@ -17,11 +19,13 @@ impl MitosGuiState {
         compositor_state: CompositorState,
         xdg_shell_state: XdgShellState,
         shm_state: ShmState,
+        seat_state: SeatState<Self>,
     ) -> Self {
         Self {
             compositor_state,
             xdg_shell_state,
             shm_state,
+            seat_state,
         }
     }
 }
