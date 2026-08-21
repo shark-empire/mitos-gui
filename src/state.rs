cfdd1825828eb72bb7d3f862b81fff5dbd81adbb
Shark-empire <babor3331@icloud.com>
2026-08-21 20:11:54 +0000

    Add SHM state to MitosGuiState structure

--- src/state.rs
+++ src/state.rs
@@ -1,21 +1,27 @@
 //! Global state for the MITOS compositor.
 
-use smithay::wayland::compositor::CompositorState;
-use smithay::wayland::shell::xdg::XdgShellState;
+use smithay::wayland::{
+    compositor::CompositorState,
+    shm::ShmState,
+    shell::xdg::XdgShellState,
+};
 
 pub struct MitosGuiState {
     pub compositor_state: CompositorState,
     pub xdg_shell_state: XdgShellState,
+    pub shm_state: ShmState,
 }
 
 impl MitosGuiState {
     pub fn new(
         compositor_state: CompositorState,
         xdg_shell_state: XdgShellState,
+        shm_state: ShmState,
     ) -> Self {
         Self {
             compositor_state,
             xdg_shell_state,
+            shm_state,
         }
     }
 }
