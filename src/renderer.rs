7af59d6d47b9dc19004bed2eb9c48201989614b6
Shark-empire <babor3331@icloud.com>
2026-08-24 15:43:40 +0000

    Add AsRenderElements to renderer imports

--- src/renderer.rs
+++ src/renderer.rs
@@ -20,6 +20,7 @@ use smithay::{
             render_elements,
             solid::{SolidColorBuffer, SolidColorRenderElement},
             surface::WaylandSurfaceRenderElement,
+            AsRenderElements,
         },
         gles::{
             element::PixelShaderElement,
